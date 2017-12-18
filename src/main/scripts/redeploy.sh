d3ed470b153ef44d4a20dc61a8aaf520cdeebc2d
Shamaila Mahmood <swardi2001@hotmail.com>
2017-12-18 12:12:07 +0500

    Updated spring profile name in deployment script

--- src/main/scripts/redeploy.sh
+++ src/main/scripts/redeploy.sh
@@ -2,4 +2,4 @@
 git pull
 $(pwd)/mvnw clean package -DskipTests=true
 kill -9 $(cat application.pid 2>/dev/null) 2>/dev/null
-nohup java -jar -Dspring.profiles.active=stage target/insurance-0.0.1-SNAPSHOT.jar >nohup.out  2>&1 &
+nohup java -jar -Dspring.profiles.active=mockservices target/insurance-0.0.1-SNAPSHOT.jar >nohup.out  2>&1 &
